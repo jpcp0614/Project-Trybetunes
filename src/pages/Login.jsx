678f619c2f36aad12fd78208629e2c4a578cdae1
jpcp0614 <jpcp0614@gmail.com>
2021-10-19 16:21:30 -0300

    Cria a funcao de habilitar o botao, contando os caracteres do login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      login: '',
+      disabled: true,
+    };
+  }
+
+  // Função para contar caracteres do nome no login
+  verifyCharacterInLogin() {
+    const NUMBER_MIN = 3;
+    const { login } = this.state;
+    const lengthName = login.length >= NUMBER_MIN;
+    this.setState({
+      disabled: !lengthName,
+    });
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-login">
         <form>
@@ -11,6 +30,7 @@ class Login extends Component {
           />
           <button
             data-testid="login-submit-button"
+            disabled={ disabled }
             type="button"
           >
             Entrar
