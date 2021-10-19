cb8c455776d2a7c46a28c23b3d0c83a4ee5a3d32
jpcp0614 <jpcp0614@gmail.com>
2021-10-19 20:12:00 -0300

    Requisito 3 - OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      personLogged: '', // nome da pessoa logada inicialmente
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getPersonLogged();
+  }
+
+  // Função que vai buscar o nome da pessoa logada
+  async getPersonLogged() {
+    const namePerson = await getUser(); // buscar na função getUser, o nome da pessoa usuária
+    const { name } = namePerson; // desestruturo o objeto namePerson
+    this.setState({
+      personLogged: name,
+      loading: false, // atualizo o nome da pessoa e o estado do loading
+    });
+  }
+
   render() {
+    const { personLogged, loading } = this.state;
     return (
-      <header data-testid="header-component"> </header>
+      <header data-testid="header-component">
+        <h2 data-testid="header-user-name">
+          { loading ? <span>Carregando...</span> : personLogged }
+        </h2>
+      </header>
     );
   }
 }
