4c30ebe140b6fc3a2ccd1cda2a89ec34345a6f04
jpcp0614 <jpcp0614@gmail.com>
2021-10-19 15:21:54 -0300

    Cria o componente NotFound

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit"> </div>
+    );
+  }
+}
+
+export default NotFound;
