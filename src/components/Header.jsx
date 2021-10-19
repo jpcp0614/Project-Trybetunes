6871c7bc3b3bf61c478beb8e9ea0158df11ed27c
jpcp0614 <jpcp0614@gmail.com>
2021-10-19 20:42:26 -0300

    Requisito 4 - OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -32,6 +33,11 @@ class Header extends Component {
         <h2 data-testid="header-user-name">
           { loading ? <Loading /> : personLogged }
         </h2>
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
       </header>
     );
   }
