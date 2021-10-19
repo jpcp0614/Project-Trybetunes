2edf8db9bbf8831f66f12e5407146875eb50ffa6
jpcp0614 <jpcp0614@gmail.com>
2021-10-19 15:52:58 -0300

    Cria o form com input e button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login"> </div>
+      <div data-testid="page-login">
+        <form>
+          <input
+            data-testid="login-name-input"
+            type="text"
+          />
+          <button
+            data-testid="login-submit-button"
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
