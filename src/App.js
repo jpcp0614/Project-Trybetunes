9c9dbbeec518142fa081da77c2263a90b47776f4
jpcp0614 <jpcp0614@gmail.com>
2021-10-19 15:36:42 -0300

    Crias as rotas - Requisito 1 - OK

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Search from './pages/Search';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ Login } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/search" component={ Search } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="*" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
