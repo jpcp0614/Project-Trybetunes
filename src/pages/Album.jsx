ebf4d4fd179a4c32df0cb55cb665e45cbd81c179
jpcp0614 <jpcp0614@gmail.com>
2021-10-20 02:53:17 -0300

    Coloca os comentarios, Requisito 7 - OK

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,52 +1,62 @@
 import React, { Component } from 'react';
-import getMusicsFunc from '../services/musicsAPI';
+import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
+
+// Para resolver o requisito 7, tive a ajuda dos meus amigos Denis, Arthur, Leonardo, Flavio e Fernando
 
 class Album extends Component {
   constructor() {
     super();
     this.state = {
-      musics: [],
+      musicsList: [], // inicia com array vazio
     };
-    this.showAlbumInfo = this.showAlbumInfo.bind(this);
+    this.getMusics = this.getMusics.bind(this);
   }
 
-  // 
+  componentDidMount() {
+    this.getMusics();
+  }
 
-  // Função com as informações dos álbuns
-  showAlbumInfo() {
-    const { musics } = this.state;
-    if (musics.length > 0) {
-      const imgUrl = musics[0].artworkUrl100; // imagem
-      const imgAlt = musics[0].collectionName; // nome do album
-      const artist = musics[0].artistName; // nome do artista
-      return (
-        <div>
-          <img src={ imgUrl } alt={ imgAlt } />
-          <h3 data-testid="artist-name">{ artist }</h3>
-          <p data-testid="album-name">{ imgAlt }</p>
-        </div>
-      );
-    }
+  // Função para requisitar a API
+  async getMusics() {
+    const { match: { params: { id } } } = this.props;
+
+    const response = await getMusics(id); // buscar com o id
+    this.setState({
+      musicsList: response,
+      artistName: response[0].artistName, // pega a imagem no musics
+      collectionName: response[0].collectionName, // album
+      albumImage: response[0].artworkUr100, // artista
+    });
   }
 
   render() {
+    const { musicsList, artistName, collectionName, albumImage } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        { this.showAlbumInfo() }
-        { musics.slice(1) // pegar o array do map a partir do indice 1
-          .map((track) => (
-            <MusicCard
-              key={ track.trackId }
-              trackName={ track.trackName }
-              previewUrl={ track.previewUrl }
-            />
-          )) }
+        <section>
+          {/* renderizar na tela */}
+          <img src={ albumImage } alt={ collectionName } />
+          <h4 data-testid="artist-name">{ artistName }</h4>
+          <span data-testid="album-name">{ collectionName }</span>
+        </section>
+        {musicsList
+          .slice(1) // fazer o array do map a partir do indice 1
+          .map((music, index) => <MusicCard key={ index } music={ music } />)}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Album;
