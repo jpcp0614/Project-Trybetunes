0e27dd2ce1bc3ccf7b27028bf05d2ca41ad418a9
jpcp0614 <jpcp0614@gmail.com>
2021-10-20 14:59:21 -0300

    Requisito 8 - OK

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Loading from '../Loading';
+import Loading from './Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false, // o loading aparece?
+      isChecked: false, // está marcado?
+    };
+    this.onChangeInput = this.onChangeInput.bind(this);
+  }
+
+  // Função para adicionar as músicas na lista de favoritas
+  // Mesma ideia do async await do Search
+  async onChangeInput() {
+    const { music } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    await addSong(music);
+    this.setState({
+      isLoading: false,
+      isChecked: true,
+    });
+  }
+
   render() {
-    const { music: { trackName, previewUrl } } = this.props;
+    const { music: { trackName, previewUrl, trackId } } = this.props;
+    const { isLoading, isChecked } = this.state;
+    if (isLoading) return (<Loading />); // mensagem Carregando...
     return (
       <section>
         <p>{ trackName }</p>
@@ -13,11 +39,14 @@ class MusicCard extends Component {
           O seu navegador não suporta o elemento
           <code>audio</code>
         </audio>
-        <label htmlFor="trackId">
+        <label htmlFor={ trackId }>
           Favorita
           <input
+            checked={ isChecked }
             data-testid={ `checkbox-music-${trackId}` }
+            id={ trackId }
             type="checkbox"
+            onChange={ this.onChangeInput }
           />
         </label>
       </section>
@@ -29,6 +58,7 @@ MusicCard.propTypes = {
   music: PropTypes.shape({
     trackName: PropTypes.string,
     previewUrl: PropTypes.string,
+    trackId: PropTypes.number, // adicionar PropTypes da trackId
   }).isRequired,
 };
 
