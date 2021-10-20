a7ba0b9d40d41f4727ee8c60f06f488f5e25d63f
jpcp0614 <jpcp0614@gmail.com>
2021-10-20 02:14:21 -0300

    Funcao com informacoes do album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import getMusicsFunc from '../services/musicsAPI';
 import Header from '../components/Header';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      musics: [],
+    };
+  }
+
+  // 
+
+  // Função com as informações dos álbuns
+  showAlbumInfo() {
+    const { musics } = this.state;
+    if (musics.length > 0) {
+      const imgUrl = musics[0].artworkUrl100;
+      const imgAlt = musics[0].collectionName;
+      const artist = musics[0].artistName;
+      return (
+        <div>
+          <img src={ imgUrl } alt={ imgAlt } />
+          <h3 data-testid="artist-name">{ artist }</h3>
+          <p data-testid="album-name">{ imgAlt }</p>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div data-testid="page-album">
         <Header />
+        { this.showAlbumInfo() }
       </div>
     );
   }
