c10a299252453ab8164f2a6d1cc288648a90978d
jpcp0614 <jpcp0614@gmail.com>
2021-10-19 21:10:18 -0300

    Requisito 5 - OK

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,9 +5,17 @@ class Search extends Component {
   constructor() {
     super();
     this.state = {
-      artist: '',
-      source: true,
+      artist: '', // input com o nome do artista, inicialmente vazio
+      source: true, // pesquisa está desabilitada - linha 44
     };
+    this.onChangeInputArtist = this.onChangeInputArtist.bind(this);
+  }
+
+  // Função para o Input do Pesquisar
+  onChangeInputArtist({ target }) {
+    this.setState({
+      artist: target.value, // atualizo o valor digitado no input
+    }, this.verifyArtistInput); // funcao de verificar o input com o nome do artista
   }
 
   // Função para verificar artista
@@ -16,7 +24,7 @@ class Search extends Component {
     const { artist } = this.state;
     const lengthNameArtist = artist.length >= NUMBER_MIN_CHARACTERS;
     this.setState({
-      source: !lengthNameArtist, // se for >= 3 caracteres, botao habilitado
+      source: !lengthNameArtist, // se for >= 2, habilita o botão
     });
   }
 
@@ -28,6 +36,7 @@ class Search extends Component {
         <form>
           <input
             data-testid="search-artist-input"
+            onChange={ this.onChangeInputArtist }
             type="text"
           />
 
