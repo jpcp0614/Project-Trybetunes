641b256c93e4fad73b6802f2cbda1e2119c705eb
jpcp0614 <jpcp0614@gmail.com>
2021-10-19 21:01:57 -0300

    Cria a funcao para verificar artista

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artist: '',
+      source: true,
+    };
+  }
+
+  // Função para verificar artista
+  verifyArtistInput() {
+    const NUMBER_MIN_CHARACTERS = 2;
+    const { artist } = this.state;
+    const lengthNameArtist = artist.length >= NUMBER_MIN_CHARACTERS;
+    this.setState({
+      source: !lengthNameArtist, // se for >= 3 caracteres, botao habilitado
+    });
+  }
+
   render() {
+    const { source } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+          />
+
+          <button
+            data-testid="search-artist-button"
+            disabled={ source }
+            type="submit"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
