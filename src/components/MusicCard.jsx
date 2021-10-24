955098cad2926064170555a73cc906171c026305
jpcp0614 <jpcp0614@gmail.com>
2021-10-24 12:23:02 -0300

    Refazendo o requisito 8

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Loading from './Loading';
 import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
   constructor() {
     super();
+
     this.state = {
-      isLoading: false, // o loading aparece?
-      isChecked: false, // está marcado?
+      isLoading: false, // início dos states
+      isChecked: false,
     };
-    this.onChangeInput = this.onChangeInput.bind(this);
+
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
-  // Função para adicionar as músicas na lista de favoritas
-  // Mesma ideia do async await do Search
-  async onChangeInput() {
-    const { music } = this.state;
+  // Função para requisitar a API
+  async onInputChange() {
+    const { music } = this.props;
     this.setState({
       isLoading: true,
     });
     await addSong(music);
     this.setState({
-      isLoading: false,
+      isLoading: false, // atualizar enquanto espera a API
       isChecked: true,
     });
   }
@@ -30,7 +31,7 @@ class MusicCard extends Component {
   render() {
     const { music: { trackName, previewUrl, trackId } } = this.props;
     const { isLoading, isChecked } = this.state;
-    if (isLoading) return (<Loading />); // mensagem Carregando...
+    if (isLoading) return <Loading />;
     return (
       <section>
         <p>{ trackName }</p>
@@ -42,11 +43,11 @@ class MusicCard extends Component {
         <label htmlFor={ trackId }>
           Favorita
           <input
-            checked={ isChecked }
-            data-testid={ `checkbox-music-${trackId}` }
-            id={ trackId }
             type="checkbox"
-            onChange={ this.onChangeInput }
+            id={ trackId }
+            data-testid={ `checkbox-music-${trackId}` }
+            checked={ isChecked }
+            onChange={ this.onInputChange }
           />
         </label>
       </section>
@@ -58,7 +59,7 @@ MusicCard.propTypes = {
   music: PropTypes.shape({
     trackName: PropTypes.string,
     previewUrl: PropTypes.string,
-    trackId: PropTypes.number, // adicionar PropTypes da trackId
+    trackId: PropTypes.number,
   }).isRequired,
 };
 
